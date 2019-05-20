6f34003076201d9f145278d81f307d2674bdc5de
Jason Aviles <sandman2k18@outlook.com>
2019-05-20 16:18:50 -0400

    added alittle animation worked on responsiveness for all screens

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 let hamburger = document.querySelector('.hamburger')
 let nav =  document.querySelectorAll('.navigation nav')
+let bannerBox= document.querySelector('.banner-box')
+let banner = document.querySelector('.banner')
  let red = true;
 
 hamburger.addEventListener('click', ()=>{
@@ -8,3 +10,11 @@ hamburger.addEventListener('click', ()=>{
        
     })
 })
+
+
+banner.addEventListener('mouseover',()=>{
+     bannerBox.style.left='0'
+     bannerBox.style.opacity='.8'
+})
+
+   
